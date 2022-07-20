c353a7d892733c3c41b2ff73c67d06d495fdcf31
juliamsousa <39354498+juliamsousa@users.noreply.github.com>
2022-07-20 19:50:51 -0300

    Adicionados getters das classes

--- sprint 1/dist/js/app.js
+++ sprint 1/dist/js/app.js
@@ -1,3 +1,16 @@
 import { Negociacao } from "./models/negociacao";
 
 const negociacao = new Negociacao(new Date(), 10, 100);
+
+/**
+ * isso é um erro
+ * essa propriedade é criada dinamicamente e nao corresponde
+ * a quantidade referente a negociacao
+ * 
+ * esse erro só é percebido em runtime, desse modo o Typescript ajuda no dev
+ * 
+ * o TS é um superset de JS e ajuda a capturar erros em devtime
+ *  */ 
+negociacao.quantidad = 10;
+
+console.log (negociacao.volume);
