9dc7cf1397440422830cfd9b7d9370f9a4e506ea
juliamsousa <39354498+juliamsousa@users.noreply.github.com>
2022-07-20 21:47:31 -0300

    Modulo 2 do curso finalizado

---
+++ sprint 1/README.md
@@ -0,0 +1,19 @@
+# Typescript Parte 1 - Evoluindo seu Javascript
+
+## Conteúdo das aulas
+
+### Módulo 1
+
+-  Introdução ao projeto e sua estrutura
+- Um pouco sobre módulos do ECMASCRIPT
+- Modelagem de uma Negociação em Javascript
+- Buracos em nossa modelagem por limitações da linguagem Javascript
+
+### Módulo 2
+
+- Download do TypeScript
+- Configuração do compilador e papel do tsconfig.json
+- Integração com scripts do Node.js
+- Modificadores de acesso private e public
+- Benefícios iniciais da linguagem TypeScript
+
--- sprint 1/app/app.ts
+++ sprint 1/app/app.ts
@@ -1,14 +1,18 @@
 import { Negociacao } from "./models/negociacao";
+
 const negociacao = new Negociacao(new Date(), 10, 100);
+
 /**
  * isso é um erro
  * essa propriedade é criada dinamicamente e nao corresponde
  * a quantidade referente a negociacao
- *
+ * 
  * esse erro só é percebido em runtime, desse modo o Typescript ajuda no dev
- *
+ * 
  * o TS é um superset de JS e ajuda a capturar erros em devtime
- *  */
+ *  */ 
+
 // com o TS adicionado esse erro passa a ser previsto em devtime
 // negociacao.quantidad = 10;
-console.log(negociacao.volume);
+
+console.log (negociacao.volume);
